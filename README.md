6f9cdc9946f0aecc1d6e31da9c4009c25ce256f5
John Clark <socrates37@gmail.com>
2022-07-05 06:14:14 -0600

    hello world

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # rolling-releases
+
+## Problem
+How to visualize a project's status?
+What do I/we work on next?
---
+++ app/android/app/src/main/kotlin/com/compiledpanda/rolling_releases/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.compiledpanda.rolling_releases
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
